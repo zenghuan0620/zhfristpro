b41eec835f5bef31c4b317499c04269ebf6b5b55
zenghuan0620 <3252000164@qq.com>
2020-10-14 15:45:01 +0800

    第二版

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
-
+#include<bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int a,b;
+    cin>>a>>b;
+    cout<<a+b<<endl;
+//    cout << "Hello world!" << endl;
     return 0;
 }
